acc7450fe4ef5b6c71c0b253c838203bb30f3a57
Inna Golovina <igigig978@gmail.com>
2021-04-11 17:27:09 +0300

    Updates

---
+++ precache-manifest.2cd7447044977464b8b3a8044e15edea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d9cd534f00ee750dde1e7761c2c298",
+    "url": "/react_todo_app/index.html"
+  },
+  {
+    "revision": "e8cad534e19c6a254a6d",
+    "url": "/react_todo_app/static/css/main.96f8354a.chunk.css"
+  },
+  {
+    "revision": "e56667073a4cb84d11c0",
+    "url": "/react_todo_app/static/js/2.d19f1a2d.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/react_todo_app/static/js/2.d19f1a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cad534e19c6a254a6d",
+    "url": "/react_todo_app/static/js/main.70bd2484.chunk.js"
+  },
+  {
+    "revision": "9a3d71c281c4f12a3d62",
+    "url": "/react_todo_app/static/js/runtime-main.167fad1e.js"
+  }
+]);
